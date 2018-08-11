b4821cb1fe97b00510c4ba7c1029e10c6178aa07
Albert Chen <albert.chen@me.com>
2018-08-11 21:26:12 +0800

    Bug fix

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get("/api/getbalance/:coin", async (req, res) => {
 });
 
 app.post("/api/getcoin", async (req, res) => {
-  console.log(JSON.stringify(req, null, 2));
+  console.log(req.body);
   try {
     assert(
       req.body.coin && req.body.destination && req.body.amount,
